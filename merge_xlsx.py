60f91db24536b50867ca552907e47425fb439620
won-n-only <siliver12@gmail.com>
2024-10-08 13:52:44 +0900

    feat: rename merged file

--- merge_xlsx.py
+++ merge_xlsx.py
@@ -5,7 +5,6 @@ from openpyxl.styles import Border, Side
 
 # 폴더 경로 설정
 OUTPUT_DIR = "output"  # 병합할 엑셀 파일들이 저장된 폴더
-MERGED_FILE = "merged_output.xlsx"  # 병합된 파일을 저장할 이름
 
 
 def merge_excel_files_with_border():
@@ -15,6 +14,10 @@ def merge_excel_files_with_border():
     if not excel_files:
         print("병합할 엑셀 파일이 없습니다.")
         return
+
+    # 첫 번째 파일 이름 가져오기
+    first_file_name = os.path.splitext(excel_files[0])[0]
+    merged_file = f"{first_file_name}_merged_output.xlsx"
 
     # 첫 번째 파일의 데이터프레임을 읽습니다.
     combined_df = pd.DataFrame()
@@ -33,7 +36,7 @@ def merge_excel_files_with_border():
         combined_df = pd.concat([combined_df, df], ignore_index=True)
 
     # 병합된 데이터프레임을 저장
-    output_file_path = os.path.join(OUTPUT_DIR, MERGED_FILE)
+    output_file_path = os.path.join(OUTPUT_DIR, merged_file)
     combined_df.to_excel(output_file_path, index=False, header=False)
 
     print(f"병합된 파일을 저장했습니다: {output_file_path}")
